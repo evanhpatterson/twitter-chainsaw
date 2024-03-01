1588b07b8e3830ad5f978f3d7873bdbac37b472c
Evan Patterson <ehp2095@rit.edu>
2024-03-01 16:20:37 -0500

    adding some starter data to test the embedding and model with

---
+++ load_data.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+def load_toy_data():
+    '''
+        A function for loading the toy data used
+        to see if the model is working.
+    '''
+    pass
