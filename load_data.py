d6a4b1db201dcd83b1627a6d82db8b188c651dd3
evanhpatterson <ehp2095@g.rit.edu>
2024-03-05 15:37:38 -0500

    renamed data file

--- load_data.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-def load_toy_data():
-    '''
-        A function for loading the toy data used
-        to see if the model is working.
-    '''
-    pass
