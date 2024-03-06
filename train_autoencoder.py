61050ba12339c7352bc824a2baaccd3056c136e7
evanhpatterson <ehp2095@g.rit.edu>
2024-03-06 02:15:02 -0500

    added train-test split

--- train_autoencoder.py
+++ train_autoencoder.py
@@ -3,6 +3,7 @@ from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from autoencoder import create_autoencoder
 import numpy as np
+import random
 
 
 def one_hot_encode_sequence(tokenizer: Tokenizer, sequence, max_length):
@@ -15,6 +16,8 @@ def one_hot_encode_sequence(tokenizer: Tokenizer, sequence, max_length):
 
 
 vocab_size = 1000
+
+split_fract = 0.9
 
 raw_data = pd.read_csv("data/example_data.csv")
 raw_data = list(raw_data['content'])
@@ -32,9 +35,29 @@ padded_sequences = pad_sequences(sequences, maxlen=max_length)
 output_sequences = [one_hot_encode_sequence(tokenizer=tokenizer, sequence=seq, max_length=max_length) for seq in padded_sequences]
 output_sequences = np.array(output_sequences, dtype=np.float32)
 
+# get permutation array for shuffle
+permutation_arr = [i for i in range(len(padded_sequences))]
+
+# shuffle permutation array
+random.shuffle(permutation_arr)
+
+# shuffle lists
+padded_sequences = padded_sequences[permutation_arr]
+output_sequences = output_sequences[permutation_arr]
+
+# get cutoff point
+cutoff_point = int(len(padded_sequences) * split_fract)
+
+train_x = padded_sequences[:cutoff_point]
+train_y = output_sequences[:cutoff_point]
+
+test_x = padded_sequences[cutoff_point:]
+test_y = output_sequences[cutoff_point:]
+
+
 autoencoder, encoder, decoder = create_autoencoder(vocab_size=vocab_size, max_length=max_length, latent_dim=16)
 
 autoencoder.summary()
 
-autoencoder.fit(padded_sequences, output_sequences)
+autoencoder.fit(train_x, train_y, epochs=2, validation_data=(test_x, test_y))
 
