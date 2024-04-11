b4aad5a08dd863421e03ce531246c618f1054e8a
evanhpatterson <ehp2095@g.rit.edu>
2024-04-10 20:40:28 -0400

    added words list and renamed data generator

--- gen_example_tweets.py
+++ gen_example_tweets.py
@@ -32,7 +32,7 @@ def generate_random_datetimes(length, min_date, max_date):
     return [min_datetime + timedelta(seconds=random.randint(0, int(delta.total_seconds()))) for _ in range(length)]
 
 
-def generate_example_data(num_rows):
+def generate_example_tweets(num_rows):
 
     data_dict = dict()
     data_dict["text"] = gen_tweets(num_rows, 3, 7)
